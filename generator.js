8f66bd3739be9cd97363e56836d6642b60046b05
caoyu <cy@wingoht.com>
2023-03-21 09:42:14 +0800

    package.json内容修改

--- generator.js
+++ generator.js
@@ -44,6 +44,19 @@ module.exports = (api, options, rootOptions) => {
         'postcss-comment': '^2.0.0',
         'sass': '^1.49.8',
         'sass-loader': '^8.0.2'
+      },
+      "uni-app": {
+        "scripts": {
+          "mp-dingtalk": {
+            "title": "钉钉小程序",
+            "env": {
+              "UNI_PLATFORM": "mp-alipay"
+            },
+            "define": {
+              "MP-DINGTALK": true
+            }
+          }
+        }
       }
     }
   })
@@ -57,104 +70,13 @@ module.exports = (api, options, rootOptions) => {
       }
     }
   }, { forceOverwrite: true })
-  if (options.template === 'default-ts') { // 启用 typescript
-    api.extendPackage(pkg => {
-      const isV4 = api.cliVersion.split('.')[0] === '4'
-      return {
-        dependencies: {
-          'vue-class-component': '^6.3.2',
-          'vue-property-decorator': '^8.0.0'
-        },
-        devDependencies: {
-          '@babel/plugin-syntax-typescript': '^7.2.0',
-          '@vue/cli-plugin-typescript': '~' + api.cliServiceVersion,
-          'typescript': isV4 ? '~4.1.5' : '~4.5.5'
-        }
-      }
-    })
-  }
 
   api.render(async function (files) {
     Object.keys(files).forEach(name => {
       delete files[name]
     })
 
-    const template = options.repo || options.template
-
     const base = 'src'
-    await generate(path.resolve(__dirname, './template/common'), files)
-    if (template === 'default') {
-      await generate(path.resolve(__dirname, './template/default'), files, base, rootOptions)
-    } else if (template === 'default-ts') {
-      await generate(path.resolve(__dirname, './template/common-ts'), files)
-      await generate(path.resolve(__dirname, './template/default-ts'), files, base, rootOptions)
-
-      // default-ts 模板删除 jsconfig.json
-      process.nextTick(() => {
-        const folderPath = path.resolve(process.cwd(), rootOptions.projectName)
-        const jsconfigPath = path.resolve(folderPath, './jsconfig.json')
-        const tsconfigPath = path.resolve(folderPath, './tsconfig.json')
-
-        if (fs.existsSync(jsconfigPath) && fs.existsSync(tsconfigPath)) {
-          fs.unlinkSync(jsconfigPath)
-        }
-      })
-    } else {
-      const ora = require('ora')
-      const home = require('user-home')
-      const download = require('download-git-repo')
-
-      const spinner = ora('模板下载中...')
-      spinner.start()
-
-      const tmp = path.join(home, '.uni-app/templates', template.replace(/[/:]/g, '-'), 'src')
-
-      if (fs.existsSync(tmp)) {
-        try {
-          require('rimraf').sync(tmp)
-        } catch (e) {
-          console.error(e)
-        }
-      }
-
-      await new Promise((resolve, reject) => {
-        download(template, tmp, err => {
-          spinner.stop()
-          if (err) {
-            return reject(err)
-          }
-          resolve()
-        })
-      })
-
-      // 合并模板依赖
-      const jsonPath = path.join(tmp, './package.json')
-      if (fs.existsSync(jsonPath)) {
-        try {
-          const json = fs.readFileSync(jsonPath, { encoding: 'utf-8' })
-          content = JSON.parse(json)
-          api.extendPackage(pkg => {
-            return {
-              dependencies: Object.assign({}, content.dependencies),
-              devDependencies: Object.assign({}, content.devDependencies)
-            }
-          })
-        } catch (error) {
-          console.warn('package.json merge failed')
-        }
-      }
-
-      const dirNames = ['cloudfunctions-aliyun', 'cloudfunctions-tcb']
-      dirNames.forEach(dirName => {
-        const dirPath = path.join(tmp, './', dirName)
-        if (fs.existsSync(dirPath)) {
-          fse.moveSync(dirPath, path.join(tmp, '../', dirName), {
-            overwrite: true
-          })
-        }
-      })
-
-      await generate(path.join(tmp, '../'), files, path.join(base, '../'))
-    }
+    await generate(path.resolve(__dirname, './template/default'), files, base, rootOptions)
   })
 }
